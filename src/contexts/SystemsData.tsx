109e4a098ac06c0f72bbb11103065722deb8bf50
Apanasenko pro <s@apanasenko.pro>
2023-03-02 14:51:18 +0200

    Initialize feat-drag-section-type

---
+++ src/contexts/SystemsData.tsx
@@ -0,0 +1,45 @@
+import { useSystemId } from "@/stores/context"
+import { filterA, mapR } from "@/utils"
+import { pipe } from "fp-ts/lib/function"
+import React, { Fragment, ReactNode } from "react"
+import { useSnapshot } from "valtio"
+import { SystemsData, useSystemsData } from "../data/system"
+import { createCtx } from "./utils"
+
+const [useCtx, CtxProvider, ctx] = createCtx<SystemsData>()
+
+export { useCtx as useSystemsData }
+
+export const SystemsDataContext = ctx
+
+type SystemsDataProviderProps = {
+  children?: ReactNode
+  onLoading?: ReactNode
+  onError?: ReactNode
+}
+
+export const SystemsDataProvider = ({
+  children,
+  onLoading = null,
+  onError = null,
+}: SystemsDataProviderProps) => {
+  const systemsData = useSystemsData()
+  if (!systemsData) return <Fragment>{onLoading}</Fragment>
+  if (systemsData === "error") return <Fragment>{onError}</Fragment>
+  return <CtxProvider value={systemsData}>{children}</CtxProvider>
+}
+
+export const useSystemData = () => {
+  const systemId = useSystemId()
+  const systemsData = useCtx() as unknown as {
+    [key: string]: Array<{ systemId: string }>
+  }
+
+  if (!systemId)
+    throw new Error("useSystemData called without a building selected")
+
+  return pipe(
+    systemsData,
+    mapR(filterA((v: { systemId: string }) => v.systemId === systemId))
+  ) as unknown as SystemsData
+}
