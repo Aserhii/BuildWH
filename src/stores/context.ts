77cb0847d875374f7268ef67fcc6249e9958b828
Apanasenko pro <s@apanasenko.pro>
2023-03-02 14:51:06 +0200

    Initialize make-configurable

---
+++ src/stores/context.ts
@@ -0,0 +1,36 @@
+import { proxy, useSnapshot } from "valtio"
+import * as z from "zod"
+
+export const EditModeEnum = z.enum(["MOVE_ROTATE", "STRETCH"])
+export type EditMode = z.infer<typeof EditModeEnum>
+
+type SiteContext = {
+  sidebar: boolean
+  buildingId: string | null
+  levelIndex: number | null
+  editMode: EditMode | null
+}
+
+const siteContext = proxy<SiteContext>({
+  sidebar: false,
+  buildingId: null,
+  levelIndex: null,
+  editMode: null,
+})
+
+export const useSiteContext = () => useSnapshot(siteContext)
+
+export const SiteContextModeEnum = z.enum(["SITE", "BUILDING", "LEVEL"])
+export type SiteContextMode = z.infer<typeof SiteContextModeEnum>
+
+export const useSiteContextMode = () => {
+  const { buildingId, levelIndex } = useSiteContext()
+
+  return levelIndex !== null
+    ? SiteContextModeEnum.Enum.LEVEL
+    : buildingId !== null
+    ? SiteContextModeEnum.Enum.BUILDING
+    : SiteContextModeEnum.Enum.SITE
+}
+
+export default siteContext
